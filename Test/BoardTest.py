9071ee95d91ab2807b4f6cbceaf2ad28c7d32e0a
Eva Garcia <evagarciadomingo@gmail.com>
2016-12-20 15:34:22 +0100

    Board test

--- Test/BoardTest.py
+++ Test/BoardTest.py
@@ -1,7 +1,11 @@
 import sys
-sys.path.append("../Classes/Game/")
+sys.path.append("../")
 
-#from Players import Players
+import Absolutos
 import unittest
 
 class BoardTest(unittest.TestCase):
+
+    def therearexcountries (self):
+        expected = 42
+        self.assertEqual(expected, Absolutos().maxcountries)
