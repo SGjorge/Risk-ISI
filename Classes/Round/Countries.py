78a42a671a1d7ffc6bf95ac1303d370c066ec73f
Eva Garcia <evagarciadomingo@gmail.com>
2016-12-20 16:07:40 +0100

    comprobacion de que un pais esté bien escrito

--- Classes/Round/Countries.py
+++ Classes/Round/Countries.py
@@ -5,3 +5,11 @@ class Countries(object):
     """docstring for """
     def __init__(self, arg):
         self.name = None
+
+    paises = ["Europa Occidental", "Europa del sur", "Europa del norte"]
+
+    def nameok (name):
+        if (nameok in paises):
+            return True;
+        else:
+            return False;
