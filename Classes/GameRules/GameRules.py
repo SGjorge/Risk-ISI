85bddd1ea8392d0bef63bb64083cff34f279c259
nereadelolmosanz <nereadelolmosanz@gmail.com>
2016-12-22 18:42:07 +0100

    código para test de reglas añaido

--- Classes/GameRules/GameRules.py
+++ Classes/GameRules/GameRules.py
@@ -1,2 +1,14 @@
-class GameRulesTest:
+import sys
+sys.path.append("../")
 
+from CoreVariables import CoreVariables as CV
+
+
+class GameRules:
+
+
+    @classmethod
+    def numberofplayers(self,num):
+        if  (num >= CV().minPlayers) and (num <= CV().maxPlayers):
+            return True
+        return False
