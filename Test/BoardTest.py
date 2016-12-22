bf24e715eed11512da6bfa70c2b1039b0a14e741
Eva Garcia <evagarciadomingo@gmail.com>
2016-12-22 12:18:30 +0100

    arreglada primera versión de test

--- Test/BoardTest.py
+++ Test/BoardTest.py
@@ -9,15 +9,15 @@ import unittest
 class BoardTest(unittest.TestCase):
 
     #comprueba que haya 42 paises
-    def thereare42countries (self):
+    def test_thereare42countries (self):
         expected = 42
         self.assertEqual(expected, Absolutos().maxCountries)
 
-    def namecountry (self):
+    def test_namecountry (self):
         expected = None
         self.assertEqual(expected, Countries().name)
 
-    def namevalid (self):
+    def test_namevalid (self):
         expected = True
         self.assertEqual(expected, Countries().nameok("Europa del norte"))
 
