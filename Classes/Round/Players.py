9121d6e6a0206d53b83a2a4974270443c184847e
SGjorge <dtjorge_2@hotmail.com>
2016-12-22 19:54:04 +0100

    Generado el constructor de la clase HumanPlayers

--- Classes/Round/Players.py
+++ Classes/Round/Players.py
@@ -5,23 +5,29 @@ class Players:
 
 	def __init__(self,name,battalions,color):
 		self.__NAME = name
+		print(self.__NAME)
 		self.__BATTALIONS = battalions
 		self.__COLOURID = color
 
-	@classmethod
 	def getName(self):
+		print(self.__NAME)
 		return self.__NAME
 
 	def getBattalions(self):
+		print(self.__BATTALIONS)
 		return self.__BATTALIONS
 
 	def getColourId(self):
+		print(self.__COLOURID)
 		return self.__COLOURID
+
+	def toString(self):
+		return (self.__NAME + " " + str(self.__BATTALIONS) + " " +self.__COLOURID)
 
 # derivated class HumanPlayers
 class HumanPlayers(Players):
-	def __init__(self):
-		None
+	def __init__(self,name,battalions,color):
+		super(self.__class__, self).__init__(name,battalions,color)
 
 # derivated class IAPlayers
 class IAPlayers(Players):
