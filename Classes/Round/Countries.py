55e2ec67588ee526858572b09a451aa941984844
Eva Garcia <evagarciadomingo@gmail.com>
2016-12-22 16:07:16 +0100

    clase countries abstracta con hija country

--- Classes/Round/Countries.py
+++ Classes/Round/Countries.py
@@ -2,6 +2,15 @@ import sys
 from CoreVariables import CoreVariables
 
 class Countries(object):
+    def nameok(self, name):
+        return None;
+    def countbattalion(self):
+        return 0;
+
+
+
+        
+class Country(Countries):
 
     global absolutes
 
