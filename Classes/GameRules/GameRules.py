a72e5dcb060f2b354dc7fd9b7dc758ab0113c8ca
Nerea Del Olmo Sanz <nereadelolmosanz@gmail.com>
2016-12-30 20:27:06 +0100

    Método y tabla para refuerzo de batallones por continente añadido

--- Classes/GameRules/GameRules.py
+++ Classes/GameRules/GameRules.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("../")
 
 from CoreVariables import CoreVariables as CV
-
+from Tables import Tables as T
 
 class GameRules:
 
@@ -12,3 +12,11 @@ class GameRules:
         if  (num >= CV().minPlayers) and (num <= CV().maxPlayers):
             return True
         return False
+
+    @classmethod
+    def getInitialBattalions(self,num):
+        return T.initialBattalions[num]
+
+    @classmethod
+    def getBattalionsPerContinent(self,continent):
+        return T.battallionsPerContinent[continent]
