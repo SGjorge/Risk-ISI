92ade2e5b5aa1ccd25c0b5ef2727b61cc2a420b6
Nerea Del Olmo Sanz <nereadelolmosanz@gmail.com>
2016-12-30 20:50:45 +0100

    metodo para numero de refuerzos segun el num de paises por jugador

--- Classes/GameRules/GameRules.py
+++ Classes/GameRules/GameRules.py
@@ -8,15 +8,22 @@ class GameRules:
 
 
     @classmethod
-    def numberofplayers(self,num):
-        if  (num >= CV().minPlayers) and (num <= CV().maxPlayers):
+    def numberofplayers(self,numPlayers):
+        if  (numPlayers >= CV().minPlayers) and (numPlayers <= CV().maxPlayers):
             return True
         return False
 
     @classmethod
-    def getInitialBattalions(self,num):
-        return T.initialBattalions[num]
+    def getInitialBattalions(self,numPlayers):
+        return T.initialBattalions[numPlayers]
 
     @classmethod
     def getBattalionsPerContinent(self,continent):
-        return T.battallionsPerContinent[continent]
+        return T.battalionsPerContinent[continent]
+
+    @classmethod
+    def getBattalionsPerCountries(self,numCountries):
+        numBattalions = numCountries // 3
+        if numBattalions < 3:
+            numBattalions = 3
+        return numBattalions
--- Classes/GameRules/Tables.py
+++ Classes/GameRules/Tables.py
@@ -2,4 +2,4 @@ import sys
 
 class Tables:
     initialBattalions = {3 : 35, 4 : 30, 5 : 25, 6 : 20}
-    battallionsPerContinent = {"América Norte" : 5, "Asia" : 7, "América Sur": 2, "Europa" : 5, "África" : 3, "Oceanía" : 2}
+    battalionsPerContinent = {"América Norte" : 5, "Asia" : 7, "América Sur": 2, "Europa" : 5, "África" : 3, "Oceanía" : 2}
