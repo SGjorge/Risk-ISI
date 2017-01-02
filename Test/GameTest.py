042fdac380d50be850ca66b17c63c09cdb537435
SGjorge <dtjorge_2@hotmail.com>
2017-01-02 12:21:49 +0100

    test creacion tablero

--- Test/GameTest.py
+++ Test/GameTest.py
@@ -4,6 +4,8 @@ sys.path.append("../Classes/Round/")
 from Players import Players,HumanPlayers,IAPlayers,ArrayPlayers
 from Countries import Countries
 from Game import Game
+from CoreVariables import CoreVariables
+
 import unittest
 
 class GameTest(unittest.TestCase):
@@ -16,6 +18,10 @@ class GameTest(unittest.TestCase):
 		self.assertEqual(None,Game().getcountries())
 		self.assertEqual(None,Game().getplayers())
 
+	def test_initboard(self):
+		world = game.getcountries() 
+		for (countrie,wCountrie) in zip(CoreVariables().countries,world):
+			assertEqual(countrie,wCountrie)
 
 if __name__ == '__main__':
 	unittest.main()
