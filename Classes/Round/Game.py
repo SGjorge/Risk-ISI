248add30d5b66042270cb6d4f347ed1e85501aaf
SGjorge <dtjorge_2@hotmail.com>
2017-01-02 11:46:08 +0100

    Creada la clase Game.py

---
+++ Classes/Round/Game.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("../Classes/GameRules/")
+
+from random import randint
+from GameRules import GameRules
+from CoreVariables import CoreVariables
+
+class Players:
+	def __init__():
