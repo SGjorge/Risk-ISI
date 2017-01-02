a247bc553928952c61b54bcbbf3f66dc2352bd85
SGjorge <dtjorge_2@hotmail.com>
2017-01-02 11:40:24 +0100

    creado el test de game

---
+++ Test/GameTest.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("../Classes/Round/")
+
+from Players import Players,HumanPlayers,IAPlayers,ArrayPlayers
+from Countries import Countries,
+import unittest
+
+class PlayersTest(unittest.TestCase):
+	
+
+if __name__ == '__main__':
+	unittest.main()
