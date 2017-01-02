750edea58c0a564623b45ff2d066f2a994d7af96
SGjorge <dtjorge_2@hotmail.com>
2017-01-02 11:51:53 +0100

    creado el test de game

--- Test/GameTest.py
+++ Test/GameTest.py
@@ -2,11 +2,16 @@ import sys
 sys.path.append("../Classes/Round/")
 
 from Players import Players,HumanPlayers,IAPlayers,ArrayPlayers
-from Countries import Countries,
+from Countries import Countries
+from Game import Game
 import unittest
 
 class PlayersTest(unittest.TestCase):
-	
+
+	def test_game(self):
+		game = Game()
+		self.assertEqual(None,Game())
+
 
 if __name__ == '__main__':
 	unittest.main()
