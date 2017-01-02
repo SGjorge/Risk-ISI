c3d672a7773d4c693bdf46c6992f38a5b163ef31
Nerea Del Olmo Sanz <nereadelolmosanz@gmail.com>
2017-01-02 11:14:25 +0100

    Test para numero de batallones en el ataque

--- Test/GameRulesTest.py
+++ Test/GameRulesTest.py
@@ -4,37 +4,38 @@ import sys
 sys.path.append("../Classes/GameRules/")
 
 from GameRules import GameRules
+#from Countries import Country
 import unittest
 
 class GameRulesTest(unittest.TestCase):
 
         #Checks if the number of players is ok (3-6)
-    def test_isoneplayerok(self):
+    def test_is1playerok(self):
         expected = False
         self.assertEqual(expected, GameRules.numberofplayersok(1))
 
-    def test_arefiveplayersok(self):
+    def test_are5playersok(self):
         expected = True
         self.assertEqual(expected, GameRules.numberofplayersok(5))
 
-    def test_aresevenplayersok(self):
+    def test_are7playersok(self):
         expected = False
         self.assertEqual(expected, GameRules.numberofplayersok(7))
 
         #Checks the number of initial battalions per player, based on the total number of players
-    def test_initialbattalions3(self):
+    def test_initialbattalionsfor3players(self):
         expected = 35
         self.assertEqual(expected, GameRules.getinitialbattalions(3))
 
-    def test_initialbattalions4(self):
+    def test_initialbattalionsfor4players(self):
         expected = 30
         self.assertEqual(expected, GameRules.getinitialbattalions(4))
 
-    def test_initialbattalions5(self):
+    def test_initialbattalionsfor5players(self):
         expected = 25
         self.assertEqual(expected, GameRules.getinitialbattalions(5))
 
-    def test_initialbattalions6(self):
+    def test_initialbattalionsfor6players(self):
         expected = 20
         self.assertEqual(expected, GameRules.getinitialbattalions(6))
 
@@ -75,6 +76,24 @@ class GameRulesTest(unittest.TestCase):
     def test_battalionsper37countries(self):
         expected = 12
         self.assertEqual(expected, GameRules.getbattalionspercountries(37)) 
+
+
+        #Checks if the number of battalions is ok to attack
+    def test_is1battaliontoattackok(self):
+        expected = True
+        self.assertEqual(expected, GameRules.battalionstoattackok("Islandia","Groenlandia",1))
+
+    def test_aretwobattalionstoattackok(self):
+        expected = True
+        self.assertEqual(expected, GameRules.battalionstoattackok("Islandia","Groenlandia",2))
+
+    def test_are3battalionstoattackok(self):
+        expected = True
+        self.assertEqual(expected, GameRules.battalionstoattackok("Islandia","Groenlandia",3))
+
+    def test_are4battalionstoattackok(self):
+        expected = True
+        self.assertEqual(expected, GameRules.battalionstoattackok("Islandia","Groenlandia",3))
 
 
 
