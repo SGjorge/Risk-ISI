36108113926f904da8ef0680641ac9ab26d72846
SGjorge <dtjorge_2@hotmail.com>
2017-01-22 12:13:06 +0100

    comprobación de la creación de los jugadores al inicio del juego implementado test fase 1

--- Test/GameTest.py
+++ Test/GameTest.py
@@ -10,7 +10,17 @@ import unittest
 
 class GameTest(unittest.TestCase):
 
-	def test_initgame(self):
+	global playersExpected
+	global p1
+	global p2
+	global p3
+
+	p1 = HumanPlayers("Pepe",0,"orange")
+	p2 = HumanPlayers("Ana",0,"red")
+	p3 = HumanPlayers("Yo",0,"blue")
+	playersExpected = [p1,p2,p3]
+
+	def test_initemptygame(self):
 		g1 = Game()
 		self.assertEqual(None,g1.getcountries())
 		self.assertEqual(None,g1.getplayers())
@@ -25,14 +35,25 @@ class GameTest(unittest.TestCase):
 
 	def test_initplayers(self):
 		game = Game()
-		p1 = HumanPlayers("Pepe",0,"orange")
-		p2 = HumanPlayers("Ana",0,"red")
-		p3 = HumanPlayers("Yo",0,"blue")
-		playersExpected = [p1,p2,p3]
 		game.initplayers(playersExpected)
 		players = game.getplayers()
 		for (pE,p) in zip(playersExpected,players):
 			self.assertEqual(True,pE.isequal(p))
+
+	def test_initfirstphaseincompletehumansplayer(self):
+		playersInitExpected = playersExpected
+		game = Game()
+		numPlayersExpected = 5 
+		game.firstphase(5,playersExpected)
+		self.assertEqual(numPlayersExpected,len(game.getplayers()))
+		IA1 = IAPlayers("1",0,"green")
+		IA2 = IAPlayers("2",0,"white")
+		playersInitExpected.append(IA1)
+		playersInitExpected.append(IA2)
+		players = game.getplayers()
+		for (pE,p) in zip(playersInitExpected,players):
+			self.assertEqual(True,pE.isequal(p))
+
 
 if __name__ == '__main__':
 	unittest.main()
