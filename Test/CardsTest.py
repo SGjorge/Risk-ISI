0401b2cac1833cdd00f81da7dc57bfa26292561e
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-01-24 20:43:04 +0100

    Tests para numero total de cartas

--- Test/CardsTest.py
+++ Test/CardsTest.py
@@ -2,7 +2,9 @@
 
 import sys
 sys.path.append("../Classes/GameRules")
+sys.path.append("../")
 from Cards import Cards, Infantry, Chivalry, Artillery
+from CoreVariables import CoreVariables as CV
 import unittest
 
 class CardsTest(unittest.TestCase):
@@ -22,6 +24,26 @@ class CardsTest(unittest.TestCase):
     def test_artillerycard (self):
         expected = None
         self.assertEqual(expected, Artillery(None).name)
+
+    def test_totalnumberofcards(self):
+        expected = CV().cardsTotalNum
+        self.assertEqual(expected, Cards(None).getcardstotal)
+
+    def test_totaljokers(self):
+        expected = 2
+        self.assertEqual(expected, Cards(None).gettotal)
+
+    def test_totalinfantery(self):
+        expected = 14
+        self.assertEqual(expected, Infantery(None).gettotal)
+
+    def test_totalchivalry(self):
+        expected = 14
+        self.assertEqual(expected, Chivalry(None).gettotal)
+
+    def test_totalartillery(self):
+        expected = 14
+        self.assertEqual(expected, Artillery(None).gettotal)
 
 
 if __name__ == '__main__':
