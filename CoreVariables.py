9cabcbbf5e6f74b8ad0c03ee050c622a6eff4d10
edo015 <edo015@ifilab53-lnx.stud.cs.uit.no>
2017-01-24 10:58:11 +0100

    Vecinos de asia

--- CoreVariables.py
+++ CoreVariables.py
@@ -28,6 +28,7 @@ class CoreVariables():
     counANorte = ['Alaska', 'Territorio del noroeste', 'Groenlandia', 'Alberta', 'Ontario', 'Quebec',
                 'Estados Unidos del oeste', 'Estados Unidos del este', 'América central']
     tableContinents = {'Europa': counEuropa, 'Asia': counAsia, 'Oceanía': counOceania, 'África': counAfrica, 'América del sur': counASur, 'América del norte': counANorte}
+    #neighbours
     #europa
     neighboursEuropaNorte = ['Europa del Sur', 'Europa occidental','Ucrania', 'Escandinavia', "Gran Bretaña"]
     neighboursEuropaSur = ['Europa del norte', 'Europa occidental', "Ucrania", 'África del norte', 'Egipto', 'Oriente medio']
@@ -53,6 +54,19 @@ class CoreVariables():
     neighboursNG = ['Indonesia', 'Australia oriental','Australia occidental']
     neighboursAOriental = ['Nueva Guinea', 'Australia occidental']
     neighboursAOcc = ['Indonesia', 'Nueva Guinea', 'Australia oriental']
+    #Asia
+    neighboursUral = ['Ucrania', 'Siberia', 'China', 'Afganistán']
+    neighboursSib = ['Ural', 'Yakutsk', 'Irkutsk', 'Mongolia', 'China']
+    neighboursYak = ['Siberia', 'Kamchatka', 'Irkutsk']
+    neighboursKam = ['Yakutsk', 'Irkutsk', 'Mongolia', 'Japón', 'Alaska']
+    neighboursIrk = ['Siberia', 'Yakutsk', 'Kamchatka', 'Mongolia']
+    neighboursJap = ['Mongolia', 'Kamchatka']
+    neighboursMong = ['Siberia', 'Irkutsk', 'Kamchatka', 'Japón', 'China']
+    neighboursChi = ['Afganistán', 'Ural', 'Siberia', 'Mongolia', 'Siam', 'India']
+    neighboursAf = ['Ucrania', 'Ural', 'China', 'India', 'Oriente medio']
+    neighboursOM = ['Europa del sur', 'Ucrania', 'Afganistán', 'India', 'África oriental', 'Egipto']
+    neighboursInd = ['Oriente medio', 'Afganistán', 'China', 'Siam']
+    neighboursSiam = ['India', 'China', 'Indonesia']
     tableNeighbours = {'Europa del norte': neighboursEuropaNorte, 'Europa del sur': neighboursEuropaSur,
                         'Europa occidental': neighboursEuropaOcc, 'Gran Bretaña': neighboursGB,
                         'Islandia': neighboursIsl, 'Escandinavia': neighboursEsca, 'Ucrania': neighboursUck,
@@ -60,7 +74,11 @@ class CoreVariables():
                         'Argentina': neighboursArg, 'África del norte' : neighboursAN, 'Egipto': neighboursEgip,
                         'África oriental': neighboursAO, 'Madagascar': neighboursMAD, 'África del sur': neighboursAS,
                         'Congo': neighboursCon, 'Indonesia': neighboursIND, 'Nueva Guinea': neighboursNG,
-                        'Australia oriental': neighboursAOriental, 'Australia occidental': neighboursAOcc}
+                        'Australia oriental': neighboursAOriental, 'Australia occidental': neighboursAOcc,
+                        'Ural': neighboursUral, 'Siberia': neighboursSib, 'Yakutsk': neighboursYak,
+                        'Kamchatka': neighboursKam, 'Irkutsk': neighboursIrk, 'Japón': neighboursJap,
+                        'Mongolia': neighboursMong, 'China': neighboursChi, 'Afganistán': neighboursAf,
+                        'Oriente medio': neighboursOM, 'India': neighboursInd, 'Siam': neighboursSiam}
     initialBattalions = {3 : 35, 4 : 30, 5 : 25, 6 : 20}
     battalionsPerContinent = {"América del norte" : 5, "Asia" : 7, "América del sur": 2, "Europa" : 5, "África" : 3, "Oceanía" : 2}
     extraBattalions = {1 : 4, 2 : 6, 3 : 8, 4 : 10, 5 : 15, 6 : 20, 7 : 25, 8 : 30, 9 : 35, 10 : 40, 11 : 45, 12 : 50, 13 : 55, 14 : 60 }
