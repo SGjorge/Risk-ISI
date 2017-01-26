b759cc4739ff9d0b09053b46efd74d6146471b72
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-01-26 17:38:35 +0100

    clase cartas modificada

--- Classes/GameRules/Cards.py
+++ Classes/GameRules/Cards.py
@@ -5,8 +5,6 @@ sys.path.append("../")
 from CoreVariables import CoreVariables as CV
 
 class Cards:
-    total = 2
-
     def __init__(self):
         self.name = "joker"
 
@@ -21,21 +19,15 @@ class Cards:
 
 
 class Infantry(Cards):
-    total = 14
-
     def __init__(self):
         self.name = "infantry"
 
 
 class Chivalry(Cards):
-    total = 14
-
     def __init__(self):
         self.name = "chivalry"
 
 
 class Artillery(Cards):
-    total = 14
-
     def __init__(self):
         self.name = "artillery"
