fc52ec231c3a9d5e88de64cb23250bc4821abebf
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-01-26 21:02:42 +0100

    erratas en movebattalions tests corregidas

--- Test/GameRulesTest.py
+++ Test/GameRulesTest.py
@@ -294,10 +294,12 @@ class GameRulesTest(unittest.TestCase):
         expected = False
         self.assertEqual(expected,GameRules.cardstoexchangeok(player,"artillery"))
 
+
         #checks if the battalion movement between two counries of a player is ok
     def test_movebattalions1(self):
+        player = HumanPlayers("Pepe",10,"orange",[])
         playerCountries = [Country("Europa del norte","orage"),Country("Europa del sur","orange")]
-        player = HumanPlayers("Pepe",10,"orange",playerCountries,[])
+        player.modifyconquered(playerCountries)
         origCountry = Country("Europa del norte","orange")
         origCountry.changebattalions(10)
         destCountry = Country("Europa del sur","orage")
@@ -306,8 +308,9 @@ class GameRulesTest(unittest.TestCase):
         self.assertEqual(expected,GameRules.movebattalions(player,origCountry,destCountry,numBattalions))
 
     def test_movebattalions2(self):
+        player = HumanPlayers("Pepe",5,"orange",[])
         playerCountries = [Country("Europa del norte","orage"),Country("Europa del sur","orange")]
-        player = HumanPlayers("Pepe",5,"orange",playerCountries,[])
+        player.modifyconquered(playerCountries)
         origCountry = Country("Europa del norte","orage")
         origCountry.changebattalions(5)
         destCountry = Country("Europa del sur","orange")
@@ -316,8 +319,9 @@ class GameRulesTest(unittest.TestCase):
         self.assertEqual(expected,GameRules.movebattalions(player,origCountry,destCountry,numBattalions))
 
     def test_movebattalions3(self):
+        player = HumanPlayers("Pepe",38,"orange",[])
         playerCountries = [Country("Europa del norte","orage"),Country("Europa del sur","orange")]
-        player = HumanPlayers("Pepe",38,"orange",playerCountries,[])
+        player.modifyconquered(playerCountries)
         origCountry = Country("Europa del norte","orange")
         origCountry.changebattalions(5)
         destCountry = Country("Islandia","red")
@@ -326,8 +330,9 @@ class GameRulesTest(unittest.TestCase):
         self.assertEqual(expected,GameRules.movebattalions(player,origCountry,destCountry,numBattalions))
 
     def test_movebattalions4(self):
+        player = HumanPlayers("Pepe",38,"orange",[])
         playerCountries = [Country("Europa del norte","orange"),Country("Europa del sur","orange"),Country("África del norte","orange")]
-        player = HumanPlayers("Pepe",38,"orange",playerCountries,[])
+        player.modifyconquered(playerCountries)
         origCountry = Country("Europa del norte","orange")
         origCountry.changebattalions(5)
         destCountry = Country("África del norte","orange")
@@ -336,8 +341,9 @@ class GameRulesTest(unittest.TestCase):
         self.assertEqual(expected,GameRules.movebattalions(player,origCountry,destCountry,numBattalions))
 
     def test_movebattalions5(self):
+        player = HumanPlayers("Pepe",38,"orange",[])
         playerCountries = [Country("Europa del norte","orange"),Country("Europa del sur","orange"),Country("África del norte","orange"),Country("Brasil","orange")]
-        player = HumanPlayers("Pepe",38,"orange",playerCountries,[])
+        player.modifyconquered(playerCountries)
         origCountry = Country("Europa del norte","orange")
         origCountry.changebattalions(5)
         destCountry = Country("Brasil","orange")
